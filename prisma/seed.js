38bb21c068b08c2982ef457b1410eb89db3a6ed7
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-05-14 01:45:47 -0500

    Se creo archivo seed.js para insertar datos

---
+++ prisma/seed.js
@@ -0,0 +1,47 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+(async function main() {
+    try {
+      
+        const Captain = await prisma.Captain.upsert({
+            where: { id: 1 },
+            update: {},
+            create: {
+                name: "Capitan 1",
+                email: "example@example.com",
+                country: "Mexico",
+                state: "Colima"
+            },
+        });
+
+        const Company = await prisma.Company.upsert({
+            where: { id: 1 },
+            update: {},
+            create: {
+                name: "STOLT SEA FARM SA",
+        
+            },
+        });
+
+        const boat = await prisma.Boat.upsert({
+            where: { id: 1 },
+            update: {},
+            create: {
+                idCaptain: 1 ,
+                idCompany: 1,
+                fishingLocation: "-23.456453424234, 34.4545423",
+                capture: "Salmon rojo",
+                zarpe: "Manzanillo, Colima, Mexico"
+            },
+        });
+  
+  
+        console.log("Create 3 explorers");
+    } catch(e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
+})();
