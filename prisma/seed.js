bf41130a3cfee71e5c8fccfd0bb71622abc8b472
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-05-14 11:03:26 -0500

    eliminando variables sin usar

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,39 +3,37 @@ const prisma = new PrismaClient();
 
 (async function main() {
     try {
-        const Captain = await prisma.Captain.upsert({
+        await prisma.Captain.upsert({
             where: { id: 1 },
             update: {},
             create: {
                 name: "Capitan 1",
                 email: "example@example.com",
                 country: "Mexico",
-                state: "Colima"
+                state: "Colima",
             },
         });
 
-        const Company = await prisma.Company.upsert({
+        await prisma.Company.upsert({
             where: { id: 1 },
             update: {},
             create: {
                 name: "STOLT SEA FARM SA",
-        
             },
         });
 
-        const boat = await prisma.Boat.upsert({
+        await prisma.Boat.upsert({
             where: { id: 1 },
             update: {},
             create: {
-                idCaptain: 1 ,
+                idCaptain: 1,
                 idCompany: 1,
                 fishingLocation: "-23.456453424234, 34.4545423",
                 capture: "Salmon rojo",
-                zarpe: "Manzanillo, Colima, Mexico"
+                zarpe: "Manzanillo, Colima, Mexico",
             },
         });
-
-    } catch(e) {
+    } catch (e) {
         console.error(e);
         process.exit(1);
     } finally {
