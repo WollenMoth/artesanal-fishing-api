fa724d487f54de993d7641ae0d97657680e24d69
André Díaz <andre290800@gmail.com>
2022-05-15 01:28:10 -0500

    Creando tests para la Companies API

--- test/server.test.js
+++ test/server.test.js
@@ -90,3 +90,69 @@ describe("Unit Tests for Captains API", () => {
         expect(res.body.message).toMatch(/no se ha podido eliminar/i);
     });
 });
+
+describe("Unit Tests for Companies API", () => {
+    let id;
+
+    test("GET /api/v1/companies", async () => {
+        const res = await http.get("/api/v1/companies");
+
+        expect(res.status).toBe(200);
+        expect(res.body).toBeDefined();
+    });
+
+    test("GET /api/v1/companies/:id", async () => {
+        const res = await http.get("/api/v1/companies/1");
+
+        expect(res.status).toBe(200);
+        expect(res.body).toBeDefined();
+    });
+
+    test("POST /api/v1/companies", async () => {
+        const res = await http.post("/api/v1/companies").send({
+            name: "Company",
+        });
+
+        expect(res.status).toBe(201);
+        expect(res.body.name).toBe("Company");
+
+        id = res.body.id;
+    });
+
+    test("PUT /api/v1/companies/:id", async () => {
+        const res = await http.put(`/api/v1/companies/${id}`).send({
+            name: "Empresa",
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.body.name).toBe("Empresa");
+    });
+
+    test("DELETE /api/v1/companies/:id", async () => {
+        const res = await http.delete(`/api/v1/companies/${id}`);
+
+        expect(res.status).toBe(200);
+        expect(res.body.message).toMatch(/eliminada/i);
+    });
+
+    test("Invalid POST /api/v1/companies", async () => {
+        const res = await http.post("/api/v1/companies").send({});
+
+        expect(res.status).toBe(400);
+        expect(res.body.message).toMatch(/no se ha podido crear/i);
+    });
+
+    test("Invalid PUT /api/v1/companies/:id", async () => {
+        const res = await http.put("/api/v1/companies/1234").send({});
+
+        expect(res.status).toBe(400);
+        expect(res.body.message).toMatch(/no se ha podido actualizar/i);
+    });
+
+    test("Invalid DELETE /api/v1/companies/:id", async () => {
+        const res = await http.delete("/api/v1/companies/1234");
+
+        expect(res.status).toBe(400);
+        expect(res.body.message).toMatch(/no se ha podido eliminar/i);
+    });
+});
