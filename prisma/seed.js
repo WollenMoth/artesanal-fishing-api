5772864e465a8927b9601a7fe0b3c7614938a7a7
WollenMoth <andre290800@gmail.com>
2022-05-15 01:14:11 -0500

    Modificando seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,30 +3,40 @@ const prisma = new PrismaClient();
 
 (async function main() {
     try {
-        await prisma.Boat.create({
-            data: {
-                fishingLocation: "-23.456453424234, 34.4545423",
-                capture: "Pulpo",
-                zarpe: "Playa de Mismaloya, Jalisco",
-                captain: {
-                    create: {
-                        name: "Capitan 2",
-                        email: "example2@example.com",
-                        country: "Mexico",
-                        state: "Jalisco",
-                    },
-                },
-                company: {
-                    create: {
-                        name: "STOLT SEA FARM SA 4",
-                    },
-                },
+        await prisma.Captain.upsert({
+            where: { id: 1 },
+            update: {},
+            create: {
+                name: "Capitan 1",
+                email: "example@example.com",
+                country: "Mexico",
+                state: "Colima",
             },
         });
 
-        await prisma.propuesta.create({
+        await prisma.Company.upsert({
+            where: { id: 1 },
+            update: {},
+            create: {
+                name: "STOLT SEA FARM SA",
+            },
+        });
+
+        await prisma.Boat.upsert({
+            where: { id: 1 },
+            update: {},
+            create: {
+                idCaptain: 1,
+                idCompany: 1,
+                fishingLocation: "-23.456453424234, 34.4545423",
+                capture: "Salmon rojo",
+                zarpe: "Manzanillo, Colima, Mexico",
+            },
+        });
+
+        await prisma.Propuesta.create({
             data: {
-                name: "UserRandom",
+                name: "Usuario",
                 email: "example@example.com",
                 phone: "111 111 1111",
                 proposal:
