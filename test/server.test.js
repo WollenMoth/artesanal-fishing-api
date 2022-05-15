d4c15033b36078bd3b59ac4a96a164cf273437e9
André Díaz <andre290800@gmail.com>
2022-05-15 01:39:41 -0500

    Creando tests para la Boats API

--- test/server.test.js
+++ test/server.test.js
@@ -156,3 +156,85 @@ describe("Unit Tests for Companies API", () => {
         expect(res.body.message).toMatch(/no se ha podido eliminar/i);
     });
 });
+
+describe("Unit Tests for the Boats API", () => {
+    let id;
+
+    test("GET /api/v1/boats", async () => {
+        const res = await http.get("/api/v1/boats");
+
+        expect(res.status).toBe(200);
+        expect(res.body).toBeDefined();
+    });
+
+    test("GET /api/v1/boats/:id", async () => {
+        const res = await http.get("/api/v1/boats/1");
+
+        expect(res.status).toBe(200);
+        expect(res.body).toBeDefined();
+    });
+
+    test("POST /api/v1/boats", async () => {
+        const res = await http.post("/api/v1/boats").send({
+            idCaptain: 1,
+            idCompany: 1,
+            fishingLocation: "35.019928, -40.575989",
+            capture: "Tuna",
+            zarpe: "Boca del Río, Veracruz, México",
+        });
+
+        expect(res.status).toBe(201);
+        expect(res.body.idCaptain).toBe(1);
+        expect(res.body.idCompany).toBe(1);
+        expect(res.body.fishingLocation).toBe("35.019928, -40.575989");
+        expect(res.body.capture).toBe("Tuna");
+        expect(res.body.zarpe).toBe("Boca del Río, Veracruz, México");
+
+        id = res.body.id;
+    });
+
+    test("PUT /api/v1/boats/:id", async () => {
+        const res = await http.put(`/api/v1/boats/${id}`).send({
+            idCaptain: 1,
+            idCompany: 1,
+            fishingLocation: "-40.575989, 35.019928",
+            capture: "Atún",
+            zarpe: "Boca del Río, Veracruz, México",
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.body.idCaptain).toBe(1);
+        expect(res.body.idCompany).toBe(1);
+        expect(res.body.fishingLocation).toBe("-40.575989, 35.019928");
+        expect(res.body.capture).toBe("Atún");
+        expect(res.body.zarpe).toBe("Boca del Río, Veracruz, México");
+    });
+
+    test("DELETE /api/v1/boats/:id", async () => {
+        const res = await http.delete(`/api/v1/boats/${id}`);
+
+        expect(res.status).toBe(200);
+        expect(res.body.message).toMatch(/eliminado/i);
+    });
+
+    test("Invalid POST /api/v1/boats", async () => {
+        const res = await http.post("/api/v1/boats").send({});
+
+        expect(res.status).toBe(400);
+        expect(res.body.message).toMatch(/no se ha podido crear/i);
+    });
+
+    test("Invalid PUT /api/v1/boats/:id", async () => {
+        const res = await http.put("/api/v1/boats/1234").send({});
+
+        expect(res.status).toBe(400);
+        expect(res.body.message).toMatch(/no se ha podido actualizar/i);
+    });
+
+    test("Invalid DELETE /api/v1/boats/:id", async () => {
+        const res = await http.delete("/api/v1/boats/1234");
+
+        expect(res.status).toBe(400);
+        expect(res.body.message).toMatch(/no se ha podido eliminar/i);
+    });
+});
